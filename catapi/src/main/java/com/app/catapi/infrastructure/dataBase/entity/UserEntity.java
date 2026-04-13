b884a79a840cfb46fa32c58e3e57548967d8ce0a
Jean Montoya <jemontoyaca@unal.edu.co>
2026-04-12 23:27:16 -0500

    Add constraints to UserEntity fields for validation

--- catapi/src/main/java/com/app/catapi/infrastructure/dataBase/entity/UserEntity.java
+++ catapi/src/main/java/com/app/catapi/infrastructure/dataBase/entity/UserEntity.java
@@ -21,11 +21,16 @@ public class UserEntity implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(unique = true, nullable = false)
     private String email;
+    @Column(nullable = false)
     private String password;
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private UserRole role;
+    @Column(nullable = false)
     private String firstName;
+    @Column(nullable = false)
     private String lastName;
 
     @Override
